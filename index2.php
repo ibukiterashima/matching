20fc1be0a5aa91b5d2d4dd0f52d4214447770d28
寺島伊吹 <66322574+ibukiterashima@users.noreply.github.com>
2020-07-18 18:20:15 +0900

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta name="robots" content="noindex,nofollow">
+	<title>ひとつ石ホームページ</title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="style2.css">
+</head>
+<body>
+	<header>
+    <h1>
+        <a href="index.html">空き家マッチングサービス</a>
+    </h1>
+		<nav class="pc-nav">
+			<ul>
+				<li><a href="index.html">ホーム</a></li>
+				<li><a href="signup.php">ログイン</a></li>
+			　	<li><a href="index2.html">空き家を見つける</a></li>
+
+		</nav>
+	</header>
+	<div class="main-visual">
+		 <span style="border-bottom: solid 5px white;"><h2>現在出品されいる空き家情報</h2></span>
+	</div>
+
+<input type="file" name="example" size="30">
+
+
+
+
+
+</body>
+</html>
